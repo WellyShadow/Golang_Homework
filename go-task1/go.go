00806c648d195edf19bbfe9b6c2595cf2ef5b19c
Vladyslav Koshelenko <vladyslav_koshelenko@epam.com>
2022-09-07 08:07:47 +0000

    Task1

--- go-task1/go.go
+++ go-task1/go.go
@@ -1,22 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func Error(err error) {
+	if err != nil {
+		fmt.Println("Your input incorrect. You must input unsigned integer numbers!")
+		os.Exit(0)
+	}
+}
 
 func main() {
-	input := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
-	fizzbuzz(input)
+	var number uint64
+	var amount_of_number uint64
+	fmt.Println("Input amount of numbers")
+	_, err := fmt.Scanf("%d", &amount_of_number)
+	Error(err)
+	var array []uint64
+	fmt.Println("Input array")
+	for i := uint64(0); i < amount_of_number; i++ {
+		_, err := fmt.Fscan(os.Stdin, &number)
+		Error(err)
+		array = append(array, number)
+	}
+	fmt.Println(fizzbuzz(array))
 
 }
-func fizzbuzz(number []int) {
-	for i := 0; i < len(number); i++ {
-		if number[i]%3 == 0 && number[i]%5 == 0 {
-			fmt.Println(number[i], "FizzBuzz")
-		} else if number[i]%3 == 0 {
-			fmt.Println(number[i], "Fizz")
-		} else if number[i]%5 == 0 {
-			fmt.Println(number[i], "Buzz")
-		} else {
-			fmt.Println(number[i])
+
+func fizzbuzz(number []uint64) []string {
+	var str []string
+	fmt.Println("Result:")
+	for _, numb := range number {
+		switch {
+		case numb == 0:
+			str = append(str, strconv.FormatUint(numb, 10))
+		case numb%3 == 0 && numb%5 == 0:
+			str = append(str, "FizzBuzz")
+		case numb%3 == 0:
+			str = append(str, "Fizz")
+		case numb%5 == 0:
+			str = append(str, "Buzz")
+		default:
+			str = append(str, strconv.FormatUint(numb, 10))
 		}
 	}
+	return str
 }
