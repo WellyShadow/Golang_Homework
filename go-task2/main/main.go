bc08ab122324aac5d6eea97143ff0a20f466a38f
Vladyslav Koshelenko <Vladyslav_Koshelenko@epam.com>
2022-09-12 13:28:54 +0300

    function Noempty

--- go-task2/main/main.go
+++ go-task2/main/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/WellyShadow/Golang_Homework/go-task2/pkg/checkerstring"
 )
 
 func main() {
-	s := "Vlad"
+	s := checkerstring{"Vlad"}
 	fmt.Println("Hi")
-	checkerstring.Notempty(s)
+	//s.checkerstring.Notempty()
 	fmt.Println(s)
 }
--- go-task2/pkg/checkerstring/checkerstring.go
+++ go-task2/pkg/checkerstring/checkerstring.go
@@ -10,10 +10,13 @@ package checkerstring
 // importing fmt package for basic
 // printing & scan operations
 
-//type checkerstring struct {
-//	strtocheck string
-//}
+type checkerstring struct {
+	strtocheck string
+}
 
-func Notempty(str string) string {
-	return str
+func (str checkerstring) Notempty() string {
+	if str.strtocheck == "" {
+		str.strtocheck = "Empty String"
+	}
+	return str.strtocheck
 }
