cafb8cd1317ea97fa6407dc64380884bae4ad276
Vladyslav Koshelenko <Vladyslav_Koshelenko@epam.com>
2022-09-12 11:22:23 +0300

    some changes

---
+++ go-task2/main/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	
+	"github.com/WellyShadow/Golang_Homework/go-task2/pkg/checkerstring"
+)
+
+var s string
+
+func main() {
+	fmt.Println("Hi")
+	s = checkerstring.notempty()
+
+}
