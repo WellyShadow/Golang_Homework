515a6cf7930f207e1f0c43a7e6571d42cda7d737
Vladyslav Koshelenko <Vladyslav_Koshelenko@epam.com>
2022-10-05 12:18:18 +0300

    fix pkg contoller

--- go-task4/controller/createuser.go
+++ go-task4/controller/createuser.go
@@ -1,11 +1,22 @@
 package controller
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 
-	"github.com/WellyShadow/Golang_Homework/go-task4/service"
+	"github.com/google/uuid"
 )
+
+type CreateUserRequest struct {
+	UserName string `json:"userName"`
+	Password string `json:"password"`
+}
+
+type CreateUserResponse struct {
+	Id       string `json:"id,omitempty"`
+	UserName string `json:"userName,omitempty"`
+}
 
 func Createuser(w http.ResponseWriter, req *http.Request) {
 
@@ -18,6 +29,26 @@ func Createuser(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		http.Error(w, "Internal Server Error : "+err.Error(), http.StatusInternalServerError)
 	}
-	service.CreateUser(body, w)
+	var reqBody CreateUserRequest
+
+	err = json.Unmarshal(body, &reqBody)
+
+	if err != nil {
+		http.Error(w, "Internal Server Error : "+err.Error(), http.StatusInternalServerError)
+	}
+
+	if len(reqBody.Password) >= 4 && len(reqBody.UserName) >= 8 {
+		id := uuid.New()
+		resBodyCreate := CreateUserResponse{id.String(), reqBody.UserName}
+		b, err := json.Marshal(resBodyCreate)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write(b)
+	} else {
+		http.Error(w, "Minimal length for password - 4 and for username - 8", http.StatusBadRequest)
+		return
+	}
 
 }
